1caa5bc657176b9e6b5eb58c275a0a8a9cb89a73
Musfikur Rahaman Tahanan <tahanan@iut-dhaka.edu>
2026-03-05 00:22:33 +0600

    fix: correct Navigation import in inventory page

--- admin-dashboard/src/pages/inventory.tsx
+++ admin-dashboard/src/pages/inventory.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navigation from '../components/Navigation';
+import { Navigation } from '../components/Navigation';
 import InventoryOverview from '../components/InventoryOverview';
 
 const InventoryPage: React.FC = () => {
